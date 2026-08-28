7f121ac23bf6d2dca02367fb66b6f1a66ba92321
MIGUEL VANDERLEI MARTINS JARDIM <miguel_jardim@estudante.sesisenai.org.br>
2026-08-27 21:54:02 -0300

    iniciando jogo

--- Assets/scripts/obstacle.cs
+++ Assets/scripts/obstacle.cs
@@ -1,15 +1,9 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class obstacle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -1,16 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+     void Update()
     {
         
     }
+
 }
